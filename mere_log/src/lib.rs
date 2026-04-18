1557536124f9254997a62f4b420907189322a6c3
Wilmer Zetterqvist <wilmerz@chalmers.se>
2026-04-19 00:51:01 +0200

    mere_log documentation + refactor

--- mere_log/src/lib.rs
+++ mere_log/src/lib.rs
@@ -1,27 +1,29 @@
+//! Minimal colored logging macros for MeRe engine (build-time and runtime prints).
+
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const BLUE: &str = "\x1b[34m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const RESET: &str = "\x1b[0m";
+
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {{
-        const BLUE: &str = "\x1b[34m";
-        const RESET: &str = "\x1b[0m";
-        println!("cargo:warning=\r  {}[MeRe: info]{}: {}", BLUE, RESET, format_args!($($arg)*));
+        println!("cargo:warning=\r  {}[MeRe: info]{}: {}", $crate::BLUE, $crate::RESET, format_args!($($arg)*));
     }};
 }
 
 #[macro_export]
 macro_rules! success {
     ($($arg:tt)*) => {{
-        const GREEN: &str = "\x1b[32m";
-        const RESET: &str = "\x1b[0m";
-        println!("cargo:warning=\r  {}[MeRe: success]{}: {}", GREEN, RESET, format_args!($($arg)*));
+        println!("cargo:warning=\r  {}[MeRe: success]{}: {}", $crate::GREEN, $crate::RESET, format_args!($($arg)*));
     }};
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {{
-        const YELLOW: &str = "\x1b[33m";
-        const RESET: &str = "\x1b[0m";
-        println!("cargo:warning=\r  {}[MeRe: warning]{}: {}", YELLOW, RESET, format_args!($($arg)*));
+        println!("cargo:warning=\r  {}[MeRe: warning]{}: {}", $crate::YELLOW, $crate::RESET, format_args!($($arg)*));
     }};
 }
 
@@ -32,8 +34,6 @@ macro_rules! error {
         return Err($arg.into());
     }};
     ($($arg:tt)*) => {{
-        const RED: &str = "\x1b[31m";
-        const RESET: &str = "\x1b[0m";
-        println!("cargo:warning=\r  {}[MeRe: error]{}: {}", RED, RESET, format_args!($($arg)*));
+        println!("cargo:warning=\r  {}[MeRe: error]{}: {}", $crate::RED, $crate::RESET, format_args!($($arg)*));
     }};
 }
